68c5cf2c9744815ea84b50ce0b9524bd9f9a6762
Benjamin Munsch <benjamin.munsch@googlemail.com>
2017-02-12 17:12:08 +0100

    adds initial guestbook template

---
+++ guestbook.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: colamann
+ * Date: 12.02.17
+ * Time: 16:02
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>phpGuestbook</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
