d351622e27577b116449960591b59f43b338a96b
Benjamin Munsch <benjamin.munsch@preis24.de>
2017-02-13 14:28:17 +0100

    adds PHPDoc for ModelGuestbookEntry

--- model/ModelGuestbookEntry.php
+++ model/ModelGuestbookEntry.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * ModelGuestbookEntry
+ *
+ * @author Benjamin Munsch <benjamin.munsch@googlemail.com>
+ */
 class ModelGuestbookEntry
 {
     public $id = null;
@@ -7,45 +12,73 @@ class ModelGuestbookEntry
     public $text = '';
     public $author = null;
 
+    /**
+     * @param   int $aId
+     * @return  ModelGuestbookEntry
+     */
     public function setId($aId)
     {
         $this->id = $aId;
         return $this;
     }
 
+    /**
+     * @param   int $aHeadline
+     * @return  ModelGuestbookEntry
+     */
     public function setHeadline($aHeadline)
     {
         $this->headline = (string)$aHeadline;
         return $this;
     }
 
+    /**
+     * @param   int $aText
+     * @return  ModelGuestbookEntry
+     */
     public function setText($aText)
     {
         $this->text = (string)$aText;
         return $this;
     }
 
+    /**
+     * @param   int $aAuthor
+     * @return  ModelGuestbookEntry
+     */
     public function setAuthor($aAuthor)
     {
         $this->author = (Int)$aAuthor;
         return $this;
     }
 
+    /**
+     * @return  int $id
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @return  int $headline
+     */
     public function getheadline()
     {
         return $this->headline;
     }
 
+    /**
+     * @return  int $text
+     */
     public function getText()
     {
         return $this->text;
     }
 
+    /**
+     * @return  int $author
+     */
     public function getAuthor()
     {
         return $this->author;
