a8a630393b3cc30357d3ea359b14a03e08524e78
Benjamin Munsch <benjamin.munsch@googlemail.com>
2017-02-14 14:20:59 +0100

    moves classloading to seperate file

--- guestbook.php
+++ guestbook.php
@@ -1,11 +1,7 @@
 <?php
-require_once('helper/HelperDbConnector.php');
-require_once('model/ModelDbEntity.php');
-require_once('model/ModelGuestbook.php');
-require_once('model/ModelGuestbookEntry.php');
-require_once('model/ModelUser.php');
-require_once('model/ModelGuestUser.php');
-require_once('model/ModelAdminUser.php');
+session_start();
+
+require_once('loader.php');
 ?>
 
 <!DOCTYPE html>
@@ -38,10 +34,45 @@ require_once('model/ModelAdminUser.php');
     endforeach;
     ?>
 
+    <h3>Registrieren:</h3>
+    <form action="register.php" method="post" class="register">
+        <input type="text" name="username"/><label for="username">Nutzername</label><br />
+        <input type="text" name="password"/><label for="password">Paßwort</label><br />
+        <input type="text" name="passwordRepeat"/><label for="password">Paßwort wiederholen</label><br />
+        <input type="submit" value="senden">
+        <input type="hidden" name="operation" value="register">
+    </form>
+
+    <h3>Anmelden:</h3>
+    <form action="login.php" method="post" class="login">
+        <input type="text" name="username"/><label for="username">Nutzername</label><br />
+        <input type="text" name="password"/><label for="password">Paßwort</label><br />
+        <input type="submit" value="senden">
+        <input type="hidden" name="operation" value="login">
+    </form>
+
+    <form action="logout.php" method="post" class="login">
+        <input type="submit" value="abmelden">
+        <input type="hidden" name="operation" value="logout">
+    </form>
+
     <?php
-    $user = new ModelGuestUser();
-    $user->loadByName('colamann');
-    var_dump($user);
+    if (isset($_POST['action'])) {
+        switch($_POST['action']) {
+            case 'login':
+                $_SESSION['user'] = new ModelGuestUser();
+                $_SESSION['user']->loadByName($_POST['username']);
+
+                if($_SESSION['user']->verify($_POST['password'])) {
+                    // TODO: session login
+                    break;
+                }
+                unset($_SESSION['user']);
+                break;
+        }
+    }
+
+
     ?>
 
 </div>
---
+++ loader.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('helper/HelperDbConnector.php');
+require_once('model/ModelDbEntity.php');
+require_once('model/ModelGuestbook.php');
+require_once('model/ModelGuestbookEntry.php');
+require_once('model/ModelUser.php');
+require_once('model/ModelGuestUser.php');
+require_once('model/ModelAdminUser.php');
