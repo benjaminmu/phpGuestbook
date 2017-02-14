e82769634248e614be47fa1d35b6d437f4c00aa8
Benjamin Munsch <benjamin.munsch@googlemail.com>
2017-02-15 00:49:23 +0100

    adds notice on successful login

--- pages/login.php
+++ pages/login.php
@@ -6,14 +6,15 @@ $user = new ModelUser($dbConnector);
 if (isset(($_POST['username']))) {
     $user->loadByName($_POST['username']);
     $_SESSION['user'] = $user->getId();
-    $_SESSION['notice'] = [
-        'message' => 'Sie sind jetzt angemeldet als '. strip_tags(htmlentities($_POST['username'])),
-    ];
 }
 
 if(!isset($_POST['password']) || !$user->verifyPasswd($_POST['password'])) {
     unset($_SESSION['user']);
     unset($user);
+} else {
+    $_SESSION['notice'] = [
+        'message' => 'Sie sind jetzt angemeldet als '. strip_tags(htmlentities($_POST['username'])),
+    ];
 }
 
 header('Location: guestbook.php');
