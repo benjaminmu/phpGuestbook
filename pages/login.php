59caf10af315b00142dbe231642e4791823c8480
Benjamin Munsch <benjamin.munsch@googlemail.com>
2017-02-15 00:09:53 +0100

    adds login message

--- pages/login.php
+++ pages/login.php
@@ -6,6 +6,9 @@ $user = new ModelUser($dbConnector);
 if (isset(($_POST['username']))) {
     $user->loadByName($_POST['username']);
     $_SESSION['user'] = $user->getId();
+    $_SESSION['notice'] = [
+        'message' => 'Sie sind jetzt angemeldet als '. strip_tags(htmlentities($_POST['username'])),
+    ];
 }
 
 if(!isset($_POST['password']) || !$user->verifyPasswd($_POST['password'])) {
