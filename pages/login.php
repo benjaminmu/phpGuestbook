e823d5b45cce8db644584ae7bd843fe7a7ed8d99
Benjamin Munsch <benjamin.munsch@googlemail.com>
2017-02-16 20:31:23 +0100

    leaves empty ModelUser object in $user when no user is logged in

--- pages/login.php
+++ pages/login.php
@@ -10,7 +10,7 @@ if (isset($_POST['username'])) {
 
 if (!isset($_POST['password']) || !$user->verifyPasswd($_POST['password'])) {
     unset($_SESSION['user']);
-    unset($user);
+    $user = new ModelUser($dbConnector);
 } else {
     $_SESSION['notice'] = [
         'message' => 'Sie sind jetzt angemeldet als ' . strip_tags(htmlentities($_POST['username'])),
